475ba8f4d95976376d9de0fb528746102299ee50
Mekolaos <Mekolaos@gmail.com>
2019-12-09 12:15:04 +0100

    initial app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+#Config
+DEBUG = True
+
+#Instanciation
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+#Enable CORS
+CORS(app, resources={r'/*':{'origins':'*'}})
+
+#Sanity check route
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify('ping!')
+
+if __name__ == "__main__":
+    app.run()
