962dafda14abcc21966403ae82932793758439d3
Mekolaos <Mekolaos@gmail.com>
2019-12-16 00:47:01 +0100

    fuck errors

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,8 @@
-from flask import jsonify, current_app, render_template
+from flask import render_template
 from app.main import bp
 
 @bp.route('/index')
 @bp.route('/')
 def index():
-    
     return render_template('index.html', title='Hypnotica')
 
