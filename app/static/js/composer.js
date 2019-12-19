544f5d32c0ac14e562e7897c02d8f75dda3c93fd
Mekolaos <Mekolaos@gmail.com>
2019-12-19 06:51:02 +0100

    I've had it with these mf'in notes on this mf'in tablature

--- app/static/js/composer.js
+++ app/static/js/composer.js
@@ -29,6 +29,18 @@ function draw_first_bar_of_tab(distance){
     endline.opacity = 0.5;
     tablature.push(endline)
 }
+
+
+function check_if_note_translation_in_array(note) {
+    var not = [note.x, note.y] ;
+    var item_as_string = JSON.stringify(not);
+    var contains = noteTranslate.some(function(ele){
+        return JSON.stringify(ele) === item_as_string;
+    });
+    return contains;
+}
+
+
 draw_first_bar_of_tab(15)
 var selectionSquare = two.makeRectangle(40, 140, 15, 15);
 selectionSquare.fill = "yellow";
@@ -40,35 +52,79 @@ var group = two.makeGroup(tablature);
 group.translation.set(0, two.height / 4);
 group.scale = 1;
 
+
+// notes position and value data
+var noteData = new Array;
+var noteTranslate = new Array;
+function note_to_del(){
+    var trans = [selectionSquare.translation.x, selectionSquare.translation.y]
+    for (var ix = 0; ix< noteTranslate.length; ix++){
+        if(JSON.stringify(trans) === JSON.stringify(noteTranslate[ix])){
+            var elem = noteData[ix]._matrix.elements;
+            noteTranslate.splice(ix, 1)
+            noteData.splice(ix, 1)
+            console.log("deleted")
+            return elem;
+        }
+    }
+    }
+
+//TODO: Detect and delete notes.
 document.onkeydown = function move(e) {
     e = e || window.event;
 
     switch (e.keyCode) {
         case 37:
+            if(selectionSquare.translation.x > 40){
             selectionSquare.translation.x -= 40;
-            console.log('Left key pressed');
+            
+            }
            break;
         case 38:
+            if(selectionSquare.translation.y > 65){
             selectionSquare.translation.y -= 15;
-             console.log('Up key pressed');
+            
+            }
            break;
         case 39:
             selectionSquare.translation.x += 40;
-             console.log('Right key pressed');
+            
            break;
         case 40:
+            if(selectionSquare.translation.y < 140 ){
             selectionSquare.translation.y += 15;
-             console.log('Down key pressed');
+            
+            }
            break;
      }
+     
      this.onkeypress= function(ev){
-        if(frets.includes(String.fromCharCode(ev.keyCode))) {
-            two.makeText(String.fromCharCode(ev.keyCode), selectionSquare.translation.x, selectionSquare.translation.y)
+        if(frets.includes(String.fromCharCode(ev.keyCode)) && !check_if_note_translation_in_array(selectionSquare.translation)) {
+            noteTranslate.push([selectionSquare.translation.x, selectionSquare.translation.y])
+            noteData.push(two.makeText(String.fromCharCode(ev.keyCode), selectionSquare.translation.x, selectionSquare.translation.y))
             two.update()
         }
-     }
+        else {
+                if(e.keyCode == 46 || e.keyCode == 8){
+                    
+                    var to_delete = note_to_del(selectionSquare.translation)
+                    // Our Saviour : noteData[1]._matrix.elements == two.scene._children.ids["two_17"]._matrix.elements
+                    // This is the only piece of code that truly matters.
+                    // This is the God Code.
+                    for(var obj in two.scene._children.ids){
+                        if (two.scene._children.ids[obj]._matrix.elements == to_delete){
+                            two.scene._children.ids[obj].remove()
+                        }
+                    }
+                    
+                    two.update()
+                }
+            }
+        }
      
+     
+     console.log("coord : " + selectionSquare.translation.x + " : " + selectionSquare.translation.y)
      two.update();
 }
-
+var saveData = {"noteslist":noteData, "notescoordinates":noteTranslate};
 two.update();
